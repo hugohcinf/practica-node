889480d45b1a62b4e418bb3df3eaeca3095b90e8
hugohcinf <94068243+hugohcinf@users.noreply.github.com>
2026-04-25 14:53:14 -0600

    Update app.js
    quite lo obsoleto

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const db = require('./database/database');
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/index.html');
